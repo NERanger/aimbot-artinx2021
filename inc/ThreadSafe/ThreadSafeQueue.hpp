233d079079e085e1326640bbf39f61c152cc55ad
Yonglin JING <511726241@qq.com>
2021-07-28 15:27:04 +0800

    Update thread-safe queue

--- inc/ThreadSafe/ThreadSafeQueue.hpp
+++ inc/ThreadSafe/ThreadSafeQueue.hpp
@@ -18,37 +18,43 @@ public:
         data_cond_.notify_one();
     }
 
-    void WaitAndPop(T& val){
-        std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return !data_queue_.empty();});
-        val = std::move(data_queue_.front());
-        data_queue_.pop();
+    // Notify that the producer thread stops, i.e. no more push operation 
+    void NotifyExit(){
+        exit_ = true;
+        data_cond_.notify_one();
     }
 
+    // void WaitAndPop(T& val){
+    //     std::unique_lock<std::mutex> lk(mutex_);
+    //     data_cond_.wait(lk, [this]{return !data_queue_.empty();});
+    //     val = std::move(data_queue_.front());
+    //     data_queue_.pop();
+    // }
+
     // Add if_wait to avoid infinite waiting
-    void WaitAndPop(T& val, bool if_wait){
+    void WaitAndPop(T& val){
         std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (!if_wait);});
-        if(if_wait){
+        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
+        if(!exit_){
             val = std::move(data_queue_.front());
             data_queue_.pop();
         }
     }
 
-    std::shared_ptr<T> WaitAndPop(){
-        std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return !data_queue_.empty();});
-        std::shared_ptr<T> res(
-            std::make_shared<T>(std::move(data_queue_.front())));
-        data_queue_.pop();
-        return res;
-    }
+    // std::shared_ptr<T> WaitAndPop(){
+    //     std::unique_lock<std::mutex> lk(mutex_);
+    //     data_cond_.wait(lk, [this]{return !data_queue_.empty();});
+    //     std::shared_ptr<T> res(
+    //         std::make_shared<T>(std::move(data_queue_.front())));
+    //     data_queue_.pop();
+    //     return res;
+    // }
 
     // Add if_wait to avoid infinite waiting
-    std::shared_ptr<T> WaitAndPop(bool if_wait){
+    std::shared_ptr<T> WaitAndPop(){
         std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (!if_wait);});
-        if(if_wait){
+        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
+        if(!exit_){
             std::shared_ptr<T> res(
             std::make_shared<T>(std::move(data_queue_.front())));
             data_queue_.pop();
@@ -93,6 +99,7 @@ private:
     mutable std::mutex mutex_;
     std::queue<T> data_queue_;
     std::condition_variable data_cond_;
+    bool exit_ = false;
 };
 
 }
