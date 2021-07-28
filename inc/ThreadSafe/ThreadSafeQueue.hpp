e67c4a994e2664c93dae2ad5f14433159a3e83da
Yonglin JING <511726241@qq.com>
2021-07-28 17:28:13 +0800

    update thread-safe queue

--- inc/ThreadSafe/ThreadSafeQueue.hpp
+++ inc/ThreadSafe/ThreadSafeQueue.hpp
@@ -19,48 +19,78 @@ public:
     }
 
     // Notify that the producer thread stops, i.e. no more push operation 
-    void NotifyExit(){
-        exit_ = true;
-        data_cond_.notify_one();
-    }
-
-    // void WaitAndPop(T& val){
-    //     std::unique_lock<std::mutex> lk(mutex_);
-    //     data_cond_.wait(lk, [this]{return !data_queue_.empty();});
-    //     val = std::move(data_queue_.front());
-    //     data_queue_.pop();
+    // void NotifyExit(){
+    //     exit_ = true;
+    //     data_cond_.notify_one();
     // }
 
-    // Add if_wait to avoid infinite waiting
     void WaitAndPop(T& val){
         std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
-        if(!exit_){
-            val = std::move(data_queue_.front());
-            data_queue_.pop();
-        }
+        data_cond_.wait(lk, [this]{return !data_queue_.empty();});
+        val = std::move(data_queue_.front());
+        data_queue_.pop();
     }
 
-    // std::shared_ptr<T> WaitAndPop(){
+    // Add exit_ to avoid infinite waiting
+    // void WaitAndPop(T& val){
     //     std::unique_lock<std::mutex> lk(mutex_);
-    //     data_cond_.wait(lk, [this]{return !data_queue_.empty();});
-    //     std::shared_ptr<T> res(
-    //         std::make_shared<T>(std::move(data_queue_.front())));
-    //     data_queue_.pop();
-    //     return res;
+    //     data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
+    //     if(!exit_){
+    //         val = std::move(data_queue_.front());
+    //         data_queue_.pop();
+    //     }
     // }
 
-    // Add if_wait to avoid infinite waiting
     std::shared_ptr<T> WaitAndPop(){
         std::unique_lock<std::mutex> lk(mutex_);
-        data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
-        if(!exit_){
-            std::shared_ptr<T> res(
+        data_cond_.wait(lk, [this]{return !data_queue_.empty();});
+        std::shared_ptr<T> res(
             std::make_shared<T>(std::move(data_queue_.front())));
+        data_queue_.pop();
+        return res;
+    }
+
+    // Add exit_ to avoid infinite waiting
+    // std::shared_ptr<T> WaitAndPop(){
+    //     std::unique_lock<std::mutex> lk(mutex_);
+    //     data_cond_.wait(lk, [this]{return (!data_queue_.empty()) || (exit_);});
+    //     if(!exit_){
+    //         std::shared_ptr<T> res(
+    //         std::make_shared<T>(std::move(data_queue_.front())));
+    //         data_queue_.pop();
+    //         return res;
+    //     }else{
+    //         return std::shared_ptr<T>();
+    //     }
+    // }
+
+    // return true if pop is successful
+    // return false if timeouts
+    bool PopTimeout(T &val, const std::chrono::milliseconds &timeout){
+        std::unique_lock<std::mutex> lk(mutex_);
+        std::cv_status status;
+        status = data_cond_.wait_for(lk, timeout);
+        if(status == std::cv_status::timeout){
+            return false;
+        }else if(status == std::cv_status::no_timeout && !data_queue_.empty()){
+            // The empty check is for spurious unblock
+            val = std::move(data_queue_.front());
             data_queue_.pop();
-            return res;
-        }else{
+            return true;
+        }
+
+        return false;
+    }
+
+    std::shared_ptr<T> PopTimeout(const std::chrono::milliseconds &timeout){
+        std::unique_lock<std::mutex> lk(mutex_);
+        std::cv_status status;
+        status = data_cond_.wait_for(lk, timeout);
+        if(status == std::cv_status::timeout){
             return std::shared_ptr<T>();
+        }else if(status == std::cv_status::no_timeout && !data_queue_.empty()){
+            // The empty check is for spurious unblock
+            return std::make_shared<T>(std::move(data_queue_.front()));
         }
     }
 
@@ -99,7 +129,7 @@ private:
     mutable std::mutex mutex_;
     std::queue<T> data_queue_;
     std::condition_variable data_cond_;
-    bool exit_ = false;
+    // bool exit_ = false;
 };
 
 }
